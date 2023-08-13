baba1b83601d19959602caae3f96df762287b098
pavl_g <scrappers.tm@gmail.com>
2023-08-13 13:25:02 +0300

    variables.sh: updated maven-3 version

--- helper-scripts/project-impl/variables.sh
+++ helper-scripts/project-impl/variables.sh
@@ -5,7 +5,7 @@
 sonatype_url="https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
 repository="ossrh"
 groupId="io.github.software-hardware-codesign"
-maven_version="3.9.3"
+maven_version="3.9.4"
 maven_bin="./apache-maven-$maven_version/bin/mvn"
 desktop_pomFile="./helper-scripts/project-impl/publishing/automata4j.pom"
 passphrase="avrsandbox"
