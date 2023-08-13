bc7a219b1155391f12fb76d00306295a4281671d
pavl_g <scrappers.tm@gmail.com>
2023-08-13 10:28:50 +0300

    core/state: Javadoc fixes

--- automata4j/src/main/java/com/avrsandbox/fsa/core/state/NextStateNotFoundException.java
+++ automata4j/src/main/java/com/avrsandbox/fsa/core/state/NextStateNotFoundException.java
@@ -34,17 +34,17 @@ package com.avrsandbox.fsa.core.state;
 import com.avrsandbox.fsa.core.TransitionalManager;
 
 /**
- * Loaded into the stack and disptached as a result of trying to call {@link TransitionalManager#transit(Object, TransitionListener)}
- * without assigning a next state or as a result of removing the state from the heap before transitting the system.
+ * Loaded into the stack and dispatched as a result of trying to call {@link TransitionalManager#transit(Object, TransitionListener)}
+ * without assigning a next state or as a result of removing the state from the heap before transiting the system.
  * 
  * @author pavl_g
  */
 public class NextStateNotFoundException extends NullPointerException {
     
     /**
-     * Instantiates a new exception as a result of transitting into an empty next-state.
+     * Instantiates a new exception as a result of transiting into an empty next-state.
      */
     public NextStateNotFoundException() {
-        super("Next-State of the transition is not found !");
+        super("Next-State of the transition is not found!");
     }
 }
