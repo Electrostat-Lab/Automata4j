22e3efbce8e91a3a6db2cea8d07bfdd93c84814b
pavl_g <scrappers.tm@gmail.com>
2023-08-15 02:43:25 +0300

    TransitionalListener: added class-generics

--- automata4j/src/main/java/com/avrsandbox/fsa/core/state/TransitionListener.java
+++ automata4j/src/main/java/com/avrsandbox/fsa/core/state/TransitionListener.java
@@ -38,10 +38,12 @@ import com.avrsandbox.fsa.core.TransitionalManager;
  * the call is loaded into the stack by invoking {@link TransitionalManager#transit(Object, TransitionListener)}.
  * 
  * Applications should implement this interface to listen for the invocation of the system transition.
- * 
+ *
+ * @param <I> the type for the state input
+ * @param <O> the type for the state tracer object
  * @author pavl_g
  */
-public interface TransitionListener {
+public interface TransitionListener<I, O> {
     
     /**
      * Dispatched as a result of committing a call to {@link TransitionalManager#transit(Object, TransitionListener)}.
@@ -49,10 +51,8 @@ public interface TransitionListener {
      * Applications should decide how they want to transit to other states from here by selectively assigning them 
      * based on some system conditions, the API provides a carrier for these system conditions on the tracer object
      * {@link AutoState#getStateTracer()} utilizing the abstract factory pattern.
-     * 
-     * @param <I> the type for the state input
-     * @param <O> the type for the state tracer object
+     *
      * @param presentState the current running state
      */
-    <I, O> void onTransition(final AutoState<I, O> presentState);
+    void onTransition(final AutoState<I, O> presentState);
 }
