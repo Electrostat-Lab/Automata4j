bfc1c4c67745f8c947ff6d7683361972d0e49fd2
pavl_g <scrappers.tm@gmail.com>
2023-08-15 02:43:01 +0300

    Added NextStateAssigner package-private class for better exception/error readability

---
+++ automata4j/src/main/java/com/avrsandbox/fsa/core/NextStateAssigner.java
@@ -0,0 +1,71 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2023, The AvrSandbox Project, Automata4j
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avrsandbox.fsa.core;
+
+import com.avrsandbox.fsa.core.state.AutoState;
+import com.avrsandbox.fsa.core.state.TransitionListener;
+import com.avrsandbox.fsa.util.TransitionPath;
+
+/**
+ * Assigns a next state {@link AutoState} when the
+ * transition from a present state from a transition path finishes.
+ *
+ * @param <I> the input type
+ * @param <O> the tracer object (output) type
+ * @author pavl_g
+ */
+class NextStateAssigner<I, O> implements TransitionListener<I, O> {
+
+    private final TransitionalManager<I, O> transitionalManager;
+    private final TransitionPath<AutoState<I, O>> transitionPath;
+    private final TransitionListener<I, O> delegator;
+
+    public NextStateAssigner(TransitionalManager<I, O> transitionalManager,
+                             TransitionPath<AutoState<I, O>> transitionPath,
+                             TransitionListener<I, O> delegator) {
+        this.transitionalManager = transitionalManager;
+        this.transitionPath = transitionPath;
+        this.delegator = delegator;
+    }
+
+    @Override
+    public void onTransition(AutoState<I, O> presentState) {
+        assert (transitionalManager != null);
+        assert (transitionPath != null);
+
+        transitionalManager.assignNextState(transitionPath.getNextState());
+        /* incremental dispatch */
+        if (delegator != null) {
+            delegator.onTransition(presentState);
+        }
+    }
+}
