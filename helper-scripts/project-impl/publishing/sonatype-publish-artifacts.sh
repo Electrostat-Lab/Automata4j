61e921c86f97cb3c10aba8f4565d92bd19989735
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-08-26 12:31:39 +0300

    sonatype-publish-artifacts.sh: added a concatenate command to check the generated pomfile

--- helper-scripts/project-impl/publishing/sonatype-publish-artifacts.sh
+++ helper-scripts/project-impl/publishing/sonatype-publish-artifacts.sh
@@ -11,6 +11,7 @@ sources_jar="${build_dir}/${desktop_artifactId_release}-${version}-sources.jar"
 javadoc_jar="${build_dir}/${desktop_artifactId_release}-${version}-javadoc.jar"
 
 generateGenericPom ${groupId} ${desktop_artifactId_release} ${version} "${desktop_pomFile}"
+cat "${desktop_pomFile}"
 
 # publish 'android' and 'desktop' builds to maven sonatype
 publishBuild "${desktop_artifactId_release}" "${desktop_artifact}" "${version}" "${javadoc_jar}" "${sources_jar}" "${desktop_pomFile}"
